a385306fbefd8d634fdfa101b32bb8a50f767d31
Yoyae <gingin.fr@gmail.com>
2026-02-05 13:38:18 +0100

    Fix build error

--- src/components/layout/PassiveDocumentationDemo.tsx
+++ src/components/layout/PassiveDocumentationDemo.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
 import { DotIcon, leonardIcons } from "@/components/ui/LeonardIcons";
 
 export function PassiveDocumentationDemo() {
